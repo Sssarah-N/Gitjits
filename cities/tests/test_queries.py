49e547d4aee6ca2c3c663dfd19feff5943e952c3
serenanam <73319962+serenanam@users.noreply.github.com>
2025-10-07 12:27:16 -0400

    move tests to cities folder

--- cities/tests/test_queries.py
+++ cities/tests/test_queries.py
@@ -1,12 +1,13 @@
 import pytest
 
-import cities.queries as qry
+import queries as qry
 
 
 def test_good_create():
-    old_rec_count = len(qry.cities)  # let's correct this!
-    assert qry.create(qry.SAMPLE_CITY) >= 1
-    assert len(qry.cities) > old_rec_count
+    old_count = qry.num_cities()
+    new_rec_id = qry.create(qry.SAMPLE_CITY)
+    assert qry.is_valid_id(new_rec_id)
+    assert qry.num_cities() > old_count
 
 
 def test_create_bad_name():

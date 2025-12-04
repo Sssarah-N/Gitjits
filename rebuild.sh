7ecb1546a42a14d3f725f5061f95c8b9ad5d3bb9
serenanam <73319962+serenanam@users.noreply.github.com>
2025-12-04 12:44:39 -0500

    add prod_env to makefile

--- rebuild.sh
+++ rebuild.sh
@@ -2,20 +2,19 @@
 # This runs on PythonAnywhere servers: fetches new code,
 # installs needed packages, and restarts the server.
 
-touch rebuild
+
 echo "Rebuilding $PA_DOMAIN"
 
-echo "Pulling code from master"
-git pull origin master
+echo "Pulling latest code..."
+git pull origin main || git pull origin master
 
-echo "Activate the virtual env $VENV for user $PA_USER"
+echo "Activating virtualenv..."
 source /home/$PA_USER/.virtualenvs/$VENV/bin/activate
 
-echo "Install packages"
+echo "Installing packages..."
 pip install --upgrade -r requirements.txt
 
-echo "Going to reboot the webserver using $API_TOKEN"
-pa_reload_webapp.py $PA_DOMAIN
+echo "Reloading web app..."
+pa_reload_webapp $PA_DOMAIN
 
-touch reboot
 echo "Finished rebuild."

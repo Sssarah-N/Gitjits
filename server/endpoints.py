004f25571565fcc1171fb8d535dfc3036384b284
serenanam <73319962+serenanam@users.noreply.github.com>
2026-04-26 22:27:05 -0400

    revert cors

--- server/endpoints.py
+++ server/endpoints.py
@@ -20,14 +20,7 @@ from server.dev_endpoints import dev_ns
 # App Setup
 # =============================================================================
 app = Flask(__name__)
-CORS(app, resources={
-    r"/*": {
-        "origins": [
-            "http://localhost:3000",
-            "https://gitjits-frontend.vercel.app"
-        ]
-    }
-})
+CORS(app)
 api = Api(app, title='Geographic Data API', version='1.0',
           description='API for managing cities, states, countries, '
                       'and national parks',
